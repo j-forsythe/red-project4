159f18a240518599bb66fcc343bac405978aa7e9
Jillian Forsythe <j.forsythe8@gmail.com>
2016-02-24 18:31:06 -0800

    Add api GET and POST

--- src/app/checkin/CheckinCtrl.js
+++ src/app/checkin/CheckinCtrl.js
@@ -6,8 +6,53 @@
     .controller('CheckinCtrl', CheckinCtrl);
 
   /** @ngInject */
-  function CheckinCtrl($scope) {
-      $scope.description = 'Angular Seed Application';
+  function CheckinCtrl($scope, $http) {
+    var JOBS_GET_URL = 'https://red-wdp-api.herokuapp.com/api/mars/jobs';
+    var COLONIST_POST_URL = 'https://red-wdp-api.herokuapp.com/api/mars/colonists';
+    //placeholder object for POST request to /colonist
+    $scope.colonist = {};
+    $scope.validate = false;
+
+    // fetch all jobs
+    $http({
+      method: 'GET',
+      url: JOBS_GET_URL
+    }).then(function(response){
+      $scope.jobs = response.data.jobs;
+    }, function(error){
+      // Handle err
+    });
+
+    $scope.login = function(event) {
+      event.preventDefault();
+
+      $http({
+        method: 'POST',
+        url: COLONIST_POST_URL,
+        data: {
+          'colonist' : $scope.colonist
+        }
+      }).then(function(response){
+        console.log(response);
+      }, function(error){
+        console.log(error);
+      });
+
+    //   //validate form
+      if ($scope.validate && event.checkinForm.$invalid) {
+        $scope.validate = false;
+      }
+      else {
+        $scope.validate = true;
+      }
+    };
+    // $scope.checkValid = function() {
+    //    if($scope.colonist) {
+    //      $scope.validate = false;
+    //    }
+
+    };
+
   }
 
 })();
--- src/app/report/ReportCtrl.js
+++ src/app/report/ReportCtrl.js
@@ -6,7 +6,35 @@
     .controller('ReportCtrl', ReportCtrl);
 
   /** @ngInject */
-  function ReportCtrl($scope) {
-  }
+  function ReportCtrl($scope, $http) {
+    var ALIENS_GET_URL = 'https://red-wdp-api.herokuapp.com/api/mars/aliens';
+    var ALIENS_POST_URL = 'https://red-wdp-api.herokuapp.com/api/mars/encounters';
+    $scope.aliens = {};
+
+    $http({
+      method: 'GET',
+      url: ALIENS_GET_URL
+    }).then(function(response){
+      $scope.aliens = response.data.aliens;
+    }, function(error){
+      console.log(error);
+    });
+
+    $scope.reportSubmit = function(event) {
+      event.preventDefault();
+
+    $http({
+      method: 'POST',
+      url: ALIENS_POST_URL,
+      data: {
+        'aliens': $scope.aliens
+      }
+    }).then(function(response){
+      console.log(response);
+    }, function(error){
+      console.log(error);
+    });
+  };
+}
 
 })();
