122df2bfbecd04ffc40eb4f8248ef71724826c8e
Jillian Forsythe <j.forsythe8@gmail.com>
2016-02-28 12:39:20 -0800

    Add header, navigation button, and style

--- src/app/encounters/EncountersCtrl.js
+++ src/app/encounters/EncountersCtrl.js
@@ -6,13 +6,17 @@
     .controller('EncountersCtrl', EncountersCtrl);
 
   /** @ngInject */
-  function EncountersCtrl($scope, $rootScope, $http, $cookies) {
+  function EncountersCtrl($scope, $rootScope, $http, $cookies, $state) {
     var ENCOUNTERS_GET_URL = 'https://red-wdp-api.herokuapp.com/api/mars/encounters';
     $scope.encounters = {};
 
     $rootScope.userInfo = {
       name: $cookies.getObject('mars_cookie').name,
       job: $cookies.getObject('mars_cookie').job.name
+    };
+
+    $scope.swipeLeft = function() {
+      $state.go('report');
     };
 
     $http({
